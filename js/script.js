87740e4bec1039a94039046d6a520e33fa9109da
AnneRosenvoldTygesen <cph-at328@stud.ek.dk>
2026-06-08 11:41:43 +0200

    forside mobil + scroll pil i bund

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const scrollHint = document.querySelector(".scroll_hint");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollHint.style.opacity = "0";
+  } else {
+    scrollHint.style.opacity = "1";
+  }
+});
+
 // const dropdownBtn = document.querySelector(".dropdown_btn");
 // const dropdownMenu = document.querySelector(".dropdown_content");
 
