fd926eeb6415a67bdec68653a10527944e9e915a
AnneRosenvoldTygesen <cph-at328@stud.ek.dk>
2026-06-08 08:48:26 +0200

    General.css header og footer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const dropdownBtn = document.querySelector(".dropdown_btn");
+// const dropdownMenu = document.querySelector(".dropdown_content");
+
+// dropdownBtn.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   dropdownMenu.classList.toggle("show");
+// });
