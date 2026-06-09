d93633d03fdc28ea9611052fe3be8a3949de297f
AnneRosenvoldTygesen <cph-at328@stud.ek.dk>
2026-06-09 19:15:20 +0200

    Færdigt site med mobil og desktop

--- js/script.js
+++ js/script.js
@@ -10,3 +10,27 @@ window.addEventListener("scroll", () => {
     scrollHint.style.opacity = "1";
   }
 });
+
+const images = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.querySelector(".close");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
